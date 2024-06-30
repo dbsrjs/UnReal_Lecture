a7a5c45bde8d528148d454b123b747a42250b883
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-30 17:41:37 +0900

    Add files via upload

---
+++ 06.30/Static/Static_2.cpp
@@ -0,0 +1,34 @@
+/*
+#include <iostream>
+using namespace std;
+
+class Person {
+public:
+	int money;//개인 소유의 돈
+	void addMoney(int money) {
+		this->money += money;
+	}
+
+	static int sharedMoney;//공금
+	static void addShard(int n) {
+		sharedMoney += n;
+	}
+};
+
+//static 변수 생성. 전연 공간에 생성
+int Person::sharedMoney = 10;//10으로 초기화
+
+//main() 함수
+int main() {
+	Person::addShard(50);//static멤버 접근, 공금 60
+	cout << Person::sharedMoney << endl;
+
+	Person han;
+	han.money = 100;
+	han.sharedMoney = 200;//static 멤버 접근 , 공금 200
+	Person::sharedMoney = 300;//static 멤버 접근, 공금 300
+	Person::addShard(100);//static 멤버 접근, 공금 400
+
+	cout << han.money << ' ' << Person::sharedMoney << endl;
+}
+*/
---
+++ 06.30/Static/Static_4.cpp
@@ -0,0 +1,35 @@
+/*
+#include <iostream>
+using namespace std;
+
+class Circle {
+private:
+	static int numOfCircles;
+	int radius;
+public:
+	Circle(int r = 1);
+	~Circle() { numOfCircles--; }//생성된 원의 개수 감도
+	double getArea() { return 3.14 * radius * radius; }
+	static int getNumOfCircles() { return numOfCircles; }
+};
+
+Circle::Circle(int r) {
+	radius = r;
+	numOfCircles++;//생성된 원의 개수 증가
+};
+int Circle::numOfCircles = 0;//0으로 초기화
+
+int main() {
+	Circle* p = new Circle[10];//10개의 생성자 실행
+	cout << "생존하고 있는 원의 개수 = " << Circle::getNumOfCircles() << endl;
+
+	delete[] p;//10개의 소멸자 실행
+	cout << "생존하고 있는 원의 개수 = " << Circle::getNumOfCircles() << endl;
+
+	Circle a;//생성자 실행
+	cout << "생존하고 있는 원의 개수 = " << Circle::getNumOfCircles() << endl;
+
+	Circle b;//생성자 실행
+	cout << "생존하고 있는 원의 개수 = " << Circle::getNumOfCircles() << endl;
+}
+*/
