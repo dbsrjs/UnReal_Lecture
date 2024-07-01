ac10d36431e03721006f500603944b40bd6550d5
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-01 11:32:14 +0900

    Create Circle_9.cpp

---
+++ 06.23/Circle/Circle_9.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+private:
+	int radius;
+public:
+	Circle();
+	Circle(int r);
+	~Circle();
+	double getArea() { return 3.14 * radius * radius; }
+	int getRadius() { return radius; }
+	void setRadius(int radius) { this->radius = radius; }
+};
+
+Circle::Circle() {
+	radius = 1;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::Circle(int radius) {
+	this->radius = radius;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::~Circle() {
+	cout << "소멸자 실행 radius = " << radius << endl;
+}
+
+void increaseCircle(Circle& c) {
+	int r = c.getRadius();
+	c.setRadius(r + 1);
+}
+
+int main() {
+	Circle waffle(30);
+	increaseCircle(waffle);
+	cout << waffle.getRadius() << endl;
+}
