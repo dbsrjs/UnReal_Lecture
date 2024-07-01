dc572b770d3375fdb95d45b9bdbc059d6e3771b6
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-01 11:27:48 +0900

    Create Circle_4.cpp

---
+++ 06.23/Circle/Circle_4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle();
+	Circle(int r);
+	~Circle();
+	void setRadius(int r) { radius = r; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+
+Circle::Circle() {
+	radius = 1;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::Circle(int r) {
+	radius = r;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::~Circle() {
+	cout << "소멸자 실행 radius = " << radius << endl;
+}
+
+int main() {
+	Circle* p, * q;
+	p = new Circle;
+	q = new Circle(30);
+	cout << p->getArea() << endl << q->getArea() << endl;
+	delete p;	//소멸자 실행
+	delete q;	//소멸자 실행
+}
