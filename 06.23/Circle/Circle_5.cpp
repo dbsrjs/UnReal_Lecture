3e17f6190b8ba2fbda1dcd71621aa86cac281a94
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-01 11:28:37 +0900

    Create Circle_5.cpp

---
+++ 06.23/Circle/Circle_5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle();
+	Circle(int r);
+	~Circle();
+	void setRadius(int r) { radius = r; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+
+Circle::Circle() {
+	radius = 1;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::Circle(int r) {
+	radius = r;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::~Circle() {
+	cout << "소멸자 실행 radius = " << radius << endl;
+}
+
+int main() {
+	int radius;
+	while (true) {
+		cout << "정수 반지름 입력(음수이면 종료)>> ";
+		cin >> radius;
+		if (radius < 0) break;//음수가 입력되면 종료
+		Circle* p = new Circle(radius);//동적 객체 생성
+		cout << "윈의 면적은 : " << p->getArea() << endl;
+		delete p;
+	}
+}
