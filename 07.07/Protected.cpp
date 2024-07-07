4b21680f98ab55071668c3a6eb3fab33ccd6bb81
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 17:53:55 +0900

    Create Protected.cpp

---
+++ 07.07/Protected.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Point {
+protected:
+	int x, y;//한 점(x, y) 좌표값
+public:
+	void set(int x, int y);
+	void showPoint();
+};
+
+void Point::set(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+
+void Point::showPoint() {
+	cout << "(" << x << "," << y << ")" << endl;
+}
+
+class ColorPoint :public Point{
+	string color;
+public:
+	void setColor(string color);
+	void showColorPoint();
+	void equals(ColorPoint p);
+};
+
+void ColorPoint::setColor(string color) {
+	this->color = color;
+}
+
+void ColorPoint::showColorPoint() {
+	cout << color << ":";
+	showPoint();
+}
+
+bool ColorPoint::equals(ColorPoint p) {
+	if (x == p.x && y == p.y && color == p.color)
+		return true;
+
+	else
+		return false;
+}
+
+
+int main() {
+	Point p;// 기본 클래스의 객체 생성
+	p.set(2, 3);
+	//p.x = 5;
+	//p.y = 5;
+	p.showPoint();
+
+	ColorPoint cp;
+	//cp.x = 10;
+	//cp.y = 10;
+	cp.set(3, 4);
+	cp.setColor("Red");
+	cp.showColorPoint();
+
+	ColorPoint cp2;
+	cp2.set(3, 4);
+	cp2.setColor("Red");
+	//cout << ((cp.equals(cp2)) ? "true" : "false");
+}
